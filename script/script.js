99ae3ade981817afb2c229515b89acdce25fd9dc
vargas22 <santiagovargasuribe2005@gmail.com>
2023-03-09 15:00:02 -0500

    proyecto 01_03

---
+++ script/script.js
@@ -0,0 +1,12 @@
+let nombre, correo ,mensajes;
+
+let formulario = document.getElementById("form")
+
+formulario.addEventListener("submit",(e)=>{
+    console.log("hola");
+    e.preventDefault();
+})
+
+function leerdatos() [
+    nombre = document.getElementById("nombre").va
+]
